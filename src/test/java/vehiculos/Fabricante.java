168de6503384fae392866bf6dcee98886a346bae
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-04-19 22:58:44 -0500

    Add files via upload

--- src/test/java/vehiculos/Fabricante.java
+++ src/test/java/vehiculos/Fabricante.java
@@ -1,3 +1,74 @@
+<<<<<<< HEAD
+	package vehiculos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Fabricante {
+	private String nombre;
+	private Pais pais;
+	private int totalFabricante;
+	private static ArrayList<Fabricante> fabricantes = new ArrayList<Fabricante>();
+	private static ArrayList<Integer> Numerofabricantes = new ArrayList<Integer>();
+	
+	
+	
+	
+	
+	public Fabricante(String nombre, Pais pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+		totalFabricante++;
+		
+		
+		
+	
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	public int getTotalFabricante() {
+		return totalFabricante;
+	}
+	public void setTotalFabricante(int totalFabricante) {
+		this.totalFabricante = totalFabricante;
+	}
+
+	
+	public static Fabricante fabricaMayorVentas() {
+		
+		
+		int supuestoMaximo = 0;
+		
+		for(int i = 1;i<Numerofabricantes.size();i++) {
+			
+			if(Numerofabricantes.get(supuestoMaximo) <Numerofabricantes.get(i)) {
+				
+				supuestoMaximo = i;
+			}
+			else{
+				
+				
+			}
+		
+			
+			
+		}
+		return fabricantes.get(supuestoMaximo);
+	}
+	
+
+}
+=======
 	package vehiculos;
 
 import java.util.ArrayList;
@@ -74,3 +145,4 @@ public class Fabricante {
 	
 
 }
+>>>>>>> a352bb6ec987fb4b8e6ef8d32fa02af700db072e
